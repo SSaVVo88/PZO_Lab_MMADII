0b0a3090bada81ffafacedfe6626406f1208125f
psawoniuk <przemyslaw.sawoniuk@gmail.com>
2024-10-14 09:52:59 +0200

    changed time to strf

--- script2.py
+++ script2.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-current_date = datetime.now()
 
-formated_date = current_date.strftime("%A,%d %B %Y %H:%M:%S")
-print(formated_date)
+
+print(datetime.now().strftime("%A,%d %B %Y %H:%M:%S"))
