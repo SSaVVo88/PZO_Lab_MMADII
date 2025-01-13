06b315f357e1042199f60f962f2ded960b52fef0
psawoniuk <przemyslaw.sawoniuk@gmail.com>
2025-01-13 09:57:14 +0100

    Nowe foldery

--- test_age.py
+++ test_age.py
@@ -5,20 +5,39 @@ from age import categorize_by_age
 class TestCategorizeByAge(unittest.TestCase):
 
     def test_child(self):
-        self.assertEqual(categorize_by_age(5), 'Child')
+
+        ages = range(11)
+
+        for age in ages:
+            with self.subTest(number=age):
+                self.assertEqual(categorize_by_age(age), 'Child')
 
     def test_teenager(self):
-        self.assertEqual(categorize_by_age(15), 'Teenager')
+
+        ages = range(11,19)
+
+        for age in ages:
+            with self.subTest(number=age):
+        self.assertEqual(categorize_by_age(age), 'Teenager')
 
     def test_adult(self):
-        self.assertEqual(categorize_by_age(30), 'Adult')
+
+        ages = range(19 - 60)
+
+        for age in ages:
+            with self.subTest(number=age):
+        self.assertEqual(categorize_by_age(age), 'Adult')
 
     def test_senior(self):
-        self.assertEqual(categorize_by_age(70), 'Senior')
+
+        ages = range(60 - 150)
+
+        for age in ages:
+            with self.subTest(number=age):
+        self.assertEqual(categorize_by_age(age), 'Senior')
 
     def test_negative_age(self):
         self.assertEqual(categorize_by_age(-5), 'Invalid age: -5.')
-
 
 
     def test_invalid_value(self):
@@ -26,6 +45,18 @@ class TestCategorizeByAge(unittest.TestCase):
         with self.assertRaises(ValueError):
             categorize_by_age('Hello')
 
+
+class TestCollection(unittest.TestCase):
+    def test_sequence_objects(self):
+        a = ('H', 'e', 'l', 'l', 'o')
+        b = 'hello'
+        self.assertAlmostEqual(a, b)
+
+def test_set_objects(self):
+    a = {1, 2, 3}
+    b = {3, 2, 1}
+    
+    self.assertSetEqual(a, b)
 
 if __name__ == '__main__':
     unittest.main()
